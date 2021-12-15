4350508040c097eb2bbe5a863e8d81320f0db023
arthurgeay <arthurgeay.contact@gmail.com>
2021-12-15 15:35:22 +0100

    feat: generate migration and model User

---
+++ app/Models/User.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public email: String
+
+  @column()
+  public password: String
+
+  @column()
+  public fullName: String
+
+  @column()
+  public providerId: String
+  
+  @column()
+  public provider: String
+  
+  @column()
+  public uuid: String
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
