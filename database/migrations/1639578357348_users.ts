0d2727f5ddaddf1768d0e8851b8a62e74e13ccb7
Arthur Geay <arthurgeay.contact@gmail.com>
2022-02-02 14:50:52 +0100

    fix: generation of UUID for users

--- database/migrations/1639578357348_users.ts
+++ database/migrations/1639578357348_users.ts
@@ -1,28 +1,32 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class Users extends BaseSchema {
-  protected tableName = 'users'
+  protected tableName = "users";
 
-  public async up () {
+  public async up() {
+    await this.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table
+        .uuid("id")
+        .primary()
+        .defaultTo(this.db.rawQuery("uuid_generate_v4()").knexQuery);
 
-      table.string('email').notNullable().unique()
-      table.string('password')
-      table.string('full_name').notNullable()
-      table.string('provider_id')
-      table.string('provider').notNullable()
-      table.uuid('uuid').notNullable()
+      table.string("email").notNullable();
+      table.string("password");
+      table.string("full_name").notNullable();
+      table.string("provider_id");
+      table.string("provider").notNullable();
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
-    })
+      table.timestamp("created_at", { useTz: true });
+      table.timestamp("updated_at", { useTz: true });
+    });
   }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
+  public async down() {
+    this.schema.dropTable(this.tableName);
   }
 }
