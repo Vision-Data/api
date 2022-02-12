b39f8589b8625201b47a683f980eabc5240da6c4
Arthur Geay <arthurgeay.contact@gmail.com>
2022-02-12 23:35:06 +0100

    feat: create task command for delete expired tokens

---
+++ app/Tasks/DeleteToken.ts
@@ -0,0 +1,23 @@
+import { BaseTask } from 'adonis5-scheduler/build'
+import Database from '@ioc:Adonis/Lucid/Database'
+
+export default class DeleteToken extends BaseTask {
+  public static get schedule() {
+    return '5 * * * * *'
+  }
+  /**
+   * Set enable use .lock file for block run retry task
+   * Lock file save to `build/tmpTaskLock`
+   */
+  public static get useLock() {
+    return false
+  }
+
+  public async handle() {
+    const deletedTokens = await Database.from('api_tokens')
+      .where('expires_at', '<', new Date())
+      .delete()
+
+    this.logger.info(`${deletedTokens} tokens deleted`)
+  }
+}
