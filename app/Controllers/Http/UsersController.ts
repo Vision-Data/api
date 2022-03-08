a50230aafa6e1b3429de9880378d07a06e5b20b4
Arthur Geay <arthurgeay.contact@gmail.com>
2022-03-08 22:38:11 +0100

    feat(user): change password (#23)
    * feat(user): change password
    
    * doc(user): change password documentation
    
    * test(user): change password

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,3 +1,4 @@
+import PasswordValidator from 'App/Validators/PasswordValidator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import UpdateUserValidator from 'App/Validators/UpdateUserValidator'
 
@@ -9,5 +10,17 @@ export default class UsersController {
   public async update({ request, auth }: HttpContextContract) {
     const payload = await request.validate(UpdateUserValidator)
     return await auth.user!.merge(payload).save()
+  }
+
+  public async updatePassword({
+    request,
+    auth,
+    response,
+  }: HttpContextContract) {
+    const payload = await request.validate(PasswordValidator)
+
+    await auth.user!.merge(payload).save()
+
+    response.status(204)
   }
 }
